b19ec532b09e532c322aa051c721df7b8023304a
Max Allfrey <max.allfrey@gmail.com>
2024-05-25 12:38:51 +0100

    Move launchers to home-manager module

---
+++ modules/home-manager/applications/gaming.nix
@@ -0,0 +1,17 @@
+{ config, lib, ... }: let
+  cfg = config.gaming;
+in
+{
+  options.gaming = {
+    enable = lib.mkEnableOption "Enables some game launchers";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      mangohud
+      lutris
+      heroic
+      bottles
+    ];
+  };
+}
