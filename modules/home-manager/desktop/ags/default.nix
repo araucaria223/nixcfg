fb70031e7c2ec43bc244b3a116bd96437acbb8d0
Max Allfrey <max.allfrey@gmail.com>
2024-08-19 23:54:48 +0100

    Re-enable ags

--- modules/home-manager/desktop/ags/default.nix
+++ modules/home-manager/desktop/ags/default.nix
@@ -2,14 +2,24 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  cfg = config.ags;
+in {
   imports = [inputs.ags.homeManagerModules.default];
 
-  programs.ags = {
-    enable = true;
-    configDir = ./.;
-    extraPackages = with pkgs; [
-      bun
-    ];
+  options.ags = {
+    enable = lib.mkEnableOption ''
+      Enable AGS widgets
+    '';
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.ags = {
+      enable = true;
+      configDir = ./.;
+      extraPackages = with pkgs; [
+        bun
+      ];
+    };
   };
 }
