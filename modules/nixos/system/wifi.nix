9fe1a05fe7759f56e3eeeb30e225a954ddcbdbf8
Max Allfrey <max.allfrey@gmail.com>
2024-08-21 16:23:30 +0100

    Improve wifi module

--- modules/nixos/system/wifi.nix
+++ modules/nixos/system/wifi.nix
@@ -1,9 +1,11 @@
-{ config, lib, pkgs, ... }:
-
-let
-  cfg = config.wifi;
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.wifi;
+in {
   options.wifi = {
     enable = lib.mkEnableOption "Enables wifi";
   };
@@ -16,11 +18,20 @@ in
       userControlled.enable = true;
       environmentFile = config.sops.secrets."wireless.env".path;
 
-      networks = {
-	"@home_ssid@".psk = "@home_psk@";
+      networks = with builtins; let
+        ntwk = name: {
+          "@${name}_ssid@".psk = "@${name}_psk";
+        };
+      in
+        listToAttrs [
+          (ntwk "home1")
+          (ntwk "home2")
+          (ntwk "hotspot")
+        ];
+      #"@home1_ssid@".psk = "@home1_psk@";
+      #"@home2_ssid@".psk = "@home2_psk@";
 
-	"@hotspot_ssid@".psk = "@hotspot_psk@";
-      };
+      #	"@hotspot_ssid@".psk = "@hotspot_psk@";
     };
   };
 }
