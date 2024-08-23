bbe209b4928e80d2dc1a792c0703abe32ca93a25
Max Allfrey <max.allfrey@gmail.com>
2024-08-23 12:25:04 +0100

    Improve flake update alias

--- modules/home-manager/terminal/zsh.nix
+++ modules/home-manager/terminal/zsh.nix
@@ -28,15 +28,17 @@ in {
       history.size = 10000;
       history.path = "${config.xdg.dataHome}/zsh/history";
 
-      shellAliases = {
+      shellAliases = let
+        flakeUp = "nix flake update ${config.home.homeDirectory}/nixos";
+      in {
         ls = "${pkgs.eza}/bin/eza";
         lsa = "${pkgs.eza}/bin/eza -la";
         lst = "${pkgs.eza}/bin/eza --tree --icons";
         cat = "${pkgs.bat}/bin/bat --theme=base16";
         f = "${pkgs.fastfetch}/bin/fastfetch --load-config examples/9.jsonc";
 
-	up = "nix flake update ${config.home.homeDirectory}/nixos";
-	reb = "sudo nixos-rebuild switch --flake ~/nixos#${settings.hostname}";
+        reb = "sudo nixos-rebuild switch --flake ~/nixos#${settings.hostname}";
+        up = "cd ${config.home.homeDirectory}/nixos && ${flakeUp} && git add flake.nix flake.lock && git commit -m 'Flake update' && reb";
       };
     };
   };
