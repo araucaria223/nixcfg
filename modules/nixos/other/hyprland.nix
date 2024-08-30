e88c6713eebefedd68296d57af73c59e951531d5
Max Allfrey <max.allfrey@gmail.com>
2024-08-30 17:22:51 +0100

    Remove unnecessary comment

--- modules/nixos/other/hyprland.nix
+++ modules/nixos/other/hyprland.nix
@@ -12,13 +12,7 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    # nix.settings = {
-    #   substituters = ["https://hyprland.cachix.org"];
-    #   trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-    # };
-
     # enable the hyprland cachix
-
     nix.settings = {
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
